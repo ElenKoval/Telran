da8d312316161108cf3786925a5dfda35d551284
Elena <elen.kova@gmail.com>
2021-01-12 18:38:08 +0100

    HomeWork 27 update

--- src/lesson27/HomeWork27.java
+++ src/lesson27/HomeWork27.java
@@ -2,10 +2,11 @@ package lesson27;
 
 public class HomeWork27 {
     public static void main(String[] args) {
-//        System.out.println(blackjack(19, 21));// → 21
-//        System.out.println(blackjack(21, 19));// → 21
-//        System.out.println(blackjack(19, 22));// → 19
-//        System.out.println(blackjack(23, 22));// → 0
+        System.out.println(blackjack(19, 21));// → 21
+        System.out.println(blackjack(21, 19));// → 21
+        System.out.println(blackjack(19, 22));// → 19
+        System.out.println(blackjack(23, 22));// → 0
+        System.out.println(blackjack(22, 19));// → 19
 //        System.out.println(loneSum(1, 2, 3));// → 6
 //        System.out.println(loneSum(3, 2, 3));// → 2
 //        System.out.println(loneSum(3, 3, 3));// → 0
@@ -15,9 +16,9 @@ public class HomeWork27 {
 //        System.out.println(starOut("sm*eilly"));// → "silly"
 //        System.out.println(starOut("sm*ei**lly"));// → "sly"
 //        System.out.println(starOut("sm*eil*ly"));// → "siy"
-        System.out.println(countHi("abc hi ho"));// → 1
-        System.out.println(countHi("ABChi hi"));// → 2
-        System.out.println(countHi("hihi"));// → 2
+//        System.out.println(countHi("abc hi ho"));// → 1
+//        System.out.println(countHi("ABChi hi"));// → 2
+//        System.out.println(countHi("hihi"));// → 2
     }
 
     private static int countHi(String str) {
@@ -55,11 +56,15 @@ public class HomeWork27 {
     }
 
     private static int blackjack(int a, int b) {
-        if (a > 21 & b > 21) {
+        int aNearest=21-a;
+        int bNearest=21-b;
+        if (a > 21 && b > 21) {
             return 0;
-        } else if (a == 21 && b <= 21) {
+        } else if (a > 21||b==21) {
+            return b;
+        } else if (b > 21 ||a==21) {
             return a;
-        } else if (b == 21 && a <= 21) {
+        }else if(aNearest>bNearest){
             return b;
         }
         return a;
